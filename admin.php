a2d3efb0039ed7cd4072998ca777fbe9c9ac8295
805ale <61597497+805ale@users.noreply.github.com>
2021-03-18 23:13:23 +0200

    Initial commit

---
+++ admin.php
@@ -0,0 +1,91 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Chollerton Tearooms | Admin page</title>
+    <!--Set viewport size and scale!-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--Linking to stylesheet-->
+    <link href="stylesheet2.css" rel="stylesheet" type="text/css">
+
+    <!--Fonts!-->
+    <!--Font - Playfair Display, Kavivanar - Linked from Google Fonts!-->
+    <link href="https://fonts.googleapis.com/css?family=Kavivanar|Playfair+Display" rel="stylesheet">
+    <!-- Font - Lobster - Linked from Google Fonts!-->
+    <link href="https://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" >
+</head>
+<body>
+<!--Header!-->
+<header>
+    <a href="index.html" class="header-brand">Chollerton Tearooms</a>
+    <!--Main navigation!-->
+    <nav>
+        <ul>
+            <li class="active"> <a href="index.html" title="home page" accesskey="h"> Home</a> </li>
+            <li> <a href="moreinfo.html" title="info" accesskey="i"> Find out more</a> </li>
+            <li> <a href="admin.php" title="admin" accesskey="a"> Admin page</a> </li>
+            <li> <a href="credits.html" title="credits" accesskey="c"> Credits page</a> </li>
+            <li> <a href="https://s3.amazonaws.com/assets.mockflow.com/app/wireframepro/fileexport/Export_Dcd988950bbaeb157bd7a34c7b6043d57.pdf" title="wireframe" accesskey="w"> Wireframe</a> </li>
+        </ul>
+    </nav>
+</header>
+<main>
+
+    <!--Banner!-->
+    <section class="index-banner">
+        <div class="vertical-center">
+            <h1>Admin page</h1>
+        </div>
+    </section>
+
+
+
+    <!-- Table section where the database results get placed into -->
+    <section class="table-wrapper">
+
+            <div id="admin-table">
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Forename</th>
+                        <th>Surname</th>
+                        <th>Address</th>
+                        <th>Landline Telephone Number</th>
+                        <th>Mobile Phone Number</th>
+                        <th>Email</th>
+                        <th>Contact Choice</th>
+                        <th>Facility Choice</th>
+                    </tr>
+                    </thead>
+
+                    <tr><td>Fred</td><td>Brown</td><td></td><td>0191 243 7613</td><td>07411111123</td><td>fred.brown@madeupemail.com</td><td>SMS</td><td>Tearoom</td></tr><tr><td>Patrick</td><td>Bloggs</td><td>15, Blogs Street, Durham, DH2 ABC</td><td>0191 123 4567</td><td>07001212111</td><td>patrick&sheila@madeupemail.com</td><td>SMS</td><td>Tearoom</td></tr><tr><td>Emma</td><td>Anderson</td><td>1 Blue Street, Newcastle Upon Tyne, NE2 3AA </td><td>0191 243 7294</td><td></td><td>emma.anderson@northumbria.ac.uk</td><td>post</td><td>Craft Shop</td></tr><tr><td>Garry</td><td>Elvin</td><td>54 Red Street, Newcastle Upon Tyne, NE2 1XE</td><td></td><td>07411111122</td><td>garry.elvin@northumbria.ac.uk</td><td>email</td><td>Village Store</td></tr><tr><td>John</td><td>Rooksby</td><td>19 White Place, Newcastle Upon Tyne, NE2 2AA</td><td>0191 227 4375</td><td>0701111111</td><td>john.rooksby@northumbria.ac.uk</td><td>email</td><td>Post Office</td></tr><tr><td>Jill</td><td>Bradnum</td><td>22 Yellow Avenue, Newcastle Upon Tyne, NE3 1AB</td><td>0191 243 7668</td><td>07771111111</td><td>jill.bradnum@northumbria.ac.uk</td><td>SMS</td><td>Bed and Breakfast</td></tr><tr><td>Sue</td><td>Jones</td><td>20 Jones Street, Newcastle Upon Tyne, NE5 1BC</td><td>0191 223 5678</td><td>07002345671</td><td></td><td>post</td><td>Bed and Breakfast</td></tr></table>                </table>
+            </div>
+    </section>
+
+</main>
+</body>
+
+
+
+<!--Footer-->
+<!--Footer-->
+<footer>
+    <!--Opening hours!-->
+    <div class="program">
+        <h4>HOURS</h4>
+        <p>Monday - Friday 8am - 11pm</p>
+        <p>Saturday 9 am - 11pm</p>
+        <p>Sunday 10am - 11pm</p>
+    </div>
+
+    <!--News section!-->
+    <div class="news">
+        <h4>NEWS</h4>
+        <p>"A tearoom and cafe for food lovers.  On December 20, Chollerton Tearooms will be unveiled to the public so keep this date free in your dairy! I cannot wait to get there!" <br> -Jane Doe for theDailyProphet.co.uk</p>
+    </div>
+    <p class="copyright">© Chollerton Tearooms <br> Designed by Alexandra Vaida </p>
+</footer>
+</body>
+</html>
+</html>
